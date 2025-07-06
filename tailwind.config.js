ffaf38e8b4ec835e5c69fbe26c418498710a177e
E_Prashanth <168891119+Eprashanth04@users.noreply.github.com>
2025-07-06 18:47:06 +0530

    css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#08d9d6"
+      }
+    }
+  },
+  plugins: []
+}
